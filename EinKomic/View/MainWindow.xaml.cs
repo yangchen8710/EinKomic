f7203e18b861ad10bee1799d4c0e90c0c533027b
yangchen <chen.yang8710@hotmail.com>
2017-10-23 17:45:55 +0800

    base code

--- EinKomic/View/MainWindow.xaml.cs
+++ EinKomic/View/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Forms;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -12,7 +13,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using EinKomic.ViewModel;
+using System.IO;
+using System.Xml.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using Emgu.CV;
 namespace EinKomic
 {
     /// <summary>
@@ -20,9 +25,46 @@ namespace EinKomic
     /// </summary>
     public partial class MainWindow : Window
     {
+        MainViewModel mvm;
         public MainWindow()
         {
             InitializeComponent();
+            mvm = new MainViewModel();
+            DataContext = mvm;
+
+        }
+
+        private void button_input_path_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog dialog = new System.Windows.Forms.FolderBrowserDialog();
+            dialog.Description = "Please Select Folder";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (string.IsNullOrEmpty(dialog.SelectedPath))
+                {
+                    return;
+                }
+                mvm.InputPath = dialog.SelectedPath;
+            }
+        }
+
+        private void button_output_path_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog dialog = new System.Windows.Forms.FolderBrowserDialog();
+            dialog.Description = "Please Select Folder";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (string.IsNullOrEmpty(dialog.SelectedPath))
+                {
+                    return;
+                }
+                mvm.OutputPath = dialog.SelectedPath;
+            }
+        }
+
+        private void comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
---
+++ EinKomic/ViewModel/MainViewModel.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EinKomic.ViewModel
+{
+    class MainViewModel : ViewModelBase
+    {
+        private string _inputPath;
+        public string InputPath
+        {
+            get
+            {
+                return _inputPath;
+            }
+            set
+            {
+                _inputPath = value;
+                OnPropertyChanged("InputPath");
+            }
+        }
+
+        private string _mainFormMessage;
+        public string MainFormMessage
+        {
+            get
+            {
+                return _mainFormMessage; 
+            }
+            set
+            {
+                _mainFormMessage = value;
+                OnPropertyChanged("MainFormMessage");
+            }
+        }
+
+        private string _outputPath;
+        public string OutputPath
+        {
+            get
+            {
+                return _outputPath;
+            }
+            set
+            {
+                _outputPath = value;
+                OnPropertyChanged("OutputPath");
+            }
+        }
+
+        private int _flipDirection;
+        public int FlipDirection
+        {
+            get
+            {
+                return _flipDirection;
+            }
+            set
+            {
+                _flipDirection = value;
+                OnPropertyChanged("FlipDirection");
+            }
+        }
+
+        private bool _singlePageCut;
+        public bool SinglePageCut
+        {
+            get
+            {
+                return _singlePageCut;
+            }
+            set
+            {
+                _singlePageCut = value;
+                MainFormMessage = "SinglePageCut:" + _singlePageCut.ToString();
+                OnPropertyChanged("SinglePageCut");
+            }
+        }
+
+    }
+}
