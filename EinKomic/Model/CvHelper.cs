15482de7e7e805caebc4e60770842aef74549c82
yangchen <chen.yang8710@hotmail.com>
2017-10-25 16:27:23 +0800

    Trim functions

--- EinKomic/Model/CvHelper.cs
+++ EinKomic/Model/CvHelper.cs
@@ -3,10 +3,232 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Emgu.CV;
 
 namespace EinKomic.Model
 {
     class CvHelper
     {
+        static int DIFF = 18;
+        public static int LeftTrim(Mat mat, Image<Emgu.CV.Structure.Gray, Byte> img)
+        {
+            int MaxCut = (int)(mat.Cols * 0.2);
+            double LeftRightIgnore = 0.2;
+            double MidIgnore = 0.1;
+            
+            Mat mat_temp = mat.Col(0);
+            
+            Image<Emgu.CV.Structure.Gray, Byte> img_temp = mat_temp.ToImage<Emgu.CV.Structure.Gray, Byte>();
+            double avg = img_temp.GetAverage().Intensity;
+            //img = mat.ToImage<Emgu.CV.Structure.Gray, Byte>();
+            int index = -1;
+            for (int i = 0; i < MaxCut; i++)
+            {
+                int count = 0;
+                for (int j = (int)(mat.Rows * LeftRightIgnore) ; j < (int)(mat.Rows * (0.5- MidIgnore/2)); j++)
+                {
+                    var a = img[j, i].Intensity;
+
+                    if (a < avg + DIFF && a > avg - DIFF)
+                    {
+
+                    }
+                    else
+                    {
+                        //Console.WriteLine(a + ":" + avg);
+                        count++;
+                    }
+                }
+
+                for (int j = (int)(mat.Rows * (0.5 + MidIgnore / 2)); j < (int)(mat.Rows * (1.0 - LeftRightIgnore)); j++)
+                {
+                    var a = img[j, i].Intensity;
+
+                    if (a < avg + DIFF && a > avg - DIFF)
+                    {
+
+                    }
+                    else
+                    {
+                        //Console.WriteLine(a + ":" + avg);
+                        count++;
+                    }
+                }
+                //Console.WriteLine("Cols:" + i + ",Count:" + +count);
+                //Console.WriteLine("Cols:" + i + ",Count:" + +count);
+                if (count > 3)
+                {
+                    index = i-1;
+                    break;
+                }
+            }
+            return index+1;
+        }
+
+        public static int TopTrim(Mat mat, Image<Emgu.CV.Structure.Gray, Byte> img)
+        {
+            int MaxCut = (int)(mat.Rows * 0.2);
+            double LeftRightIgnore = 0.2;
+            double MidIgnore = 0.1;
+
+            Mat mat_temp = mat.Row(0);
+            CvInvoke.Imshow("ak", mat_temp);
+            Image<Emgu.CV.Structure.Gray, Byte> img_temp = mat_temp.ToImage<Emgu.CV.Structure.Gray, Byte>();
+            double avg = img_temp.GetAverage().Intensity;
+            //img = mat.ToImage<Emgu.CV.Structure.Gray, Byte>();
+            int index = -1;
+            for (int i = 0; i < MaxCut; i++)
+            {
+                int count = 0;
+                for (int j = (int)(mat.Cols * LeftRightIgnore); j < (int)(mat.Cols * (0.5 - MidIgnore / 2)); j++)
+                {
+                    var a = img[i ,j].Intensity;
+
+                    if (a < avg + DIFF && a > avg - DIFF)
+                    {
+
+                    }
+                    else
+                    {
+                        //Console.WriteLine(a + ":" + avg);
+                        count++;
+                    }
+                }
+
+                for (int j = (int)(mat.Cols * (0.5 + MidIgnore / 2)); j < (int)(mat.Cols * (1.0 - LeftRightIgnore)); j++)
+                {
+                    var a = img[i, j].Intensity;
+
+                    if (a < avg + DIFF && a > avg - DIFF)
+                    {
+
+                    }
+                    else
+                    {
+                        //Console.WriteLine(a + ":" + avg);
+                        count++;
+                    }
+                }
+                //Console.WriteLine("Cols:" + i + ",Count:" + +count);
+                //Console.WriteLine("Cols:" + i + ",Count:" + +count);
+                if (count > 3)
+                {
+                    index = i-1;
+                    break;
+                }
+            }
+            return index+1;
+        }
+
+        public static int RightTrim(Mat mat, Image<Emgu.CV.Structure.Gray, Byte> img)
+        {
+            int MaxCut = (int)(mat.Cols * 0.2);
+            double LeftRightIgnore = 0.2;
+            double MidIgnore = 0.1;
+
+            Mat mat_temp = mat.Col(mat.Cols - 1);
+            Image<Emgu.CV.Structure.Gray, Byte> img_temp = mat_temp.ToImage<Emgu.CV.Structure.Gray, Byte>();
+            double avg = img_temp.GetAverage().Intensity;
+            //img = mat.ToImage<Emgu.CV.Structure.Gray, Byte>();
+            int index = mat.Cols;
+            for (int i = mat.Cols - 1; i > mat.Cols - MaxCut; i--)
+            {
+                int count = 0;
+                for (int j = (int)(mat.Rows * LeftRightIgnore); j < (int)(mat.Rows * (0.5 - MidIgnore / 2)); j++)
+                {
+                    var a = img[j, i].Intensity;
+
+                    if (a < avg + DIFF && a > avg - DIFF)
+                    {
+
+                    }
+                    else
+                    {
+                        //Console.WriteLine(a + ":" + avg);
+                        count++;
+                    }
+                }
+
+                for (int j = (int)(mat.Rows * (0.5 + MidIgnore / 2)); j < (int)(mat.Rows * (1.0 - LeftRightIgnore)); j++)
+                {
+                    var a = img[j, i].Intensity;
+
+                    if (a < avg + DIFF && a > avg - DIFF)
+                    {
+
+                    }
+                    else
+                    {
+                        //Console.WriteLine(a + ":" + avg);
+                        count++;
+                    }
+                }
+                //Console.WriteLine("Cols:" + i + ",Count:" + +count);
+                //Console.WriteLine("Cols:" + i + ",Count:" + +count);
+                if (count > 3)
+                {
+                    index = i + 1;
+                    break;
+                }
+            }
+            //Console.WriteLine("index:" + index );
+            return mat.Cols - index;
+        }
+
+        public static int BottomTrim(Mat mat, Image<Emgu.CV.Structure.Gray, Byte> img)
+        {
+            int MaxCut = (int)(mat.Cols * 0.2);
+            double LeftRightIgnore = 0.2;
+            double MidIgnore = 0.1;
+
+            Mat mat_temp = mat.Row(mat.Rows - 1);
+            Image<Emgu.CV.Structure.Gray, Byte> img_temp = mat_temp.ToImage<Emgu.CV.Structure.Gray, Byte>();
+            double avg = img_temp.GetAverage().Intensity;
+            //img = mat.ToImage<Emgu.CV.Structure.Gray, Byte>();
+            int index = mat.Rows;
+            for (int i = mat.Rows - 1; i > mat.Rows - MaxCut; i--)
+            {
+                int count = 0;
+                for (int j = (int)(mat.Cols * LeftRightIgnore); j < (int)(mat.Cols * (0.5 - MidIgnore / 2)); j++)
+                {
+                    var a = img[i, j].Intensity;
+
+                    if (a < avg + DIFF && a > avg - DIFF)
+                    {
+
+                    }
+                    else
+                    {
+                        //Console.WriteLine(a + ":" + avg);
+                        count++;
+                    }
+                }
+
+                for (int j = (int)(mat.Cols * (0.5 + MidIgnore / 2)); j < (int)(mat.Cols * (1.0 - LeftRightIgnore)); j++)
+                {
+                    var a = img[i, j].Intensity;
+
+                    if (a < avg + DIFF && a > avg - DIFF)
+                    {
+
+                    }
+                    else
+                    {
+                        //Console.WriteLine(a + ":" + avg);
+                        count++;
+                    }
+                }
+                //Console.WriteLine("Cols:" + i + ",Count:" + +count);
+                //Console.WriteLine("Cols:" + i + ",Count:" + +count);
+                if (count > 3)
+                {
+                    index = i + 1;
+                    break;
+                }
+            }
+            //Console.WriteLine("index:" + index );
+            return mat.Rows - index;
+        }
+
     }
 }
--- EinKomic/View/MainWindow.xaml.cs
+++ EinKomic/View/MainWindow.xaml.cs
@@ -1,23 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Forms;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using EinKomic.ViewModel;
-using System.IO;
-using System.Xml.Serialization;
-using System.Runtime.Serialization.Formatters.Binary;
 using Emgu.CV;
+using EinKomic.Model;
+using System.Threading;
+
 namespace EinKomic
 {
     /// <summary>
@@ -26,12 +14,80 @@ namespace EinKomic
     public partial class MainWindow : Window
     {
         MainViewModel mvm;
+        Mat mat;
+        Image<Emgu.CV.Structure.Gray, Byte> img;
+        int leftTrim, rightTrim, topTrim, bottomTrim;
         public MainWindow()
         {
             InitializeComponent();
             mvm = new MainViewModel();
             DataContext = mvm;
+            mat = CvInvoke.Imread(@"D:\pic\test1.png", Emgu.CV.CvEnum.ImreadModes.Grayscale);
+            img = mat.ToImage<Emgu.CV.Structure.Gray, Byte>();
+            //CvInvoke.Imshow("ak", mat);
+            //CvInvoke.CvtColor(mat, mat, Emgu.CV.CvEnum.ColorConversion.Bgr2Gray);
+            for (int i = 0; i < 1; i++)
+            {
+                System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
+                stopwatch.Start();
+                //任务 1...
+                if(true)
+                { 
+                Thread thread1 = new Thread(new ThreadStart(dobottom));
+                Thread thread2 = new Thread(new ThreadStart(dotop));
+                Thread thread3 = new Thread(new ThreadStart(doright));
+                Thread thread4 = new Thread(new ThreadStart(doleft));
+                thread1.Start();
+                thread2.Start();
+                thread3.Start();
+                thread4.Start();
+                thread1.Join();
+                thread2.Join();
+                thread3.Join();
+                thread4.Join();
+                }
 
+                if (false)
+                {
+                    CvHelper.RightTrim(mat,img);
+                    CvHelper.LeftTrim(mat, img);
+                    CvHelper.TopTrim(mat, img);
+                    CvHelper.BottomTrim(mat, img);
+                }
+                
+                //Console.WriteLine("Console.WriteLine(CvHelper.RightTrim(mat));:"+CvHelper.RightTrim(mat));
+                //Console.WriteLine("Console.WriteLine(CvHelper.LeftTrim(mat));:" + CvHelper.LeftTrim(mat));
+                //Console.WriteLine("Console.WriteLine(CvHelper.TopTrim(mat));:" + CvHelper.TopTrim(mat));
+                //Console.WriteLine("Console.WriteLine(CvHelper.BottomTrim(mat));:" + CvHelper.BottomTrim(mat));
+
+                stopwatch.Stop();
+                var _result = "stopwatch.ElapsedTicks ：" + stopwatch.ElapsedTicks + "。";
+                Console.WriteLine(_result);
+            }
+            //CvInvoke.Imshow("akx", img);
+            Console.WriteLine("bottomTrim:" + bottomTrim + "topTrim:" + topTrim + "rightTrim:" + rightTrim + "leftTrim:" + leftTrim);
+            System.Drawing.Rectangle rect = new System.Drawing.Rectangle(new System.Drawing.Point(leftTrim,topTrim),new System.Drawing.Size(img.Cols- rightTrim- leftTrim,img.Rows - topTrim - bottomTrim));
+            var tem = img.GetSubRect(rect);
+            CvInvoke.Imshow("akx", tem);
+
+        }
+
+        void dobottom()
+        {
+            bottomTrim = CvHelper.BottomTrim(mat, img);
+        }
+        void dotop()
+        {
+            
+            topTrim = CvHelper.TopTrim(mat, img);
+        }
+        void doright()
+        {
+            rightTrim = CvHelper.RightTrim(mat, img);
+        }
+        void doleft()
+        {
+            leftTrim = CvHelper.LeftTrim(mat, img);
         }
 
         private void button_input_path_Click(object sender, RoutedEventArgs e)
